c54bb4c3d8044e4217127dc29a2bef06b83d1af0
zHigor33 <higor_pinho@estudante.sc.senai.br>
2020-07-19 15:24:34 -0300

    Home Page

--- lib/main.dart
+++ lib/main.dart
@@ -1,38 +1,20 @@
 import 'package:flutter/material.dart';
+import './ui/doclist.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(DocExpiryApp());
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class DocExpiryApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
+        title: 'DocExpire: Higor',
+        theme: new ThemeData(primarySwatch: Colors.amber),
+        home: DocList(title: 'DocExpire: Home Page'));
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
+class DocList extends StatefulWidget {
+  DocList({Key key, this.title}) : super(key: key);
 
   // This widget is the home page of your application. It is stateful, meaning
   // that it has a State object (defined below) that contains fields that affect
@@ -46,10 +28,10 @@ class MyHomePage extends StatefulWidget {
   final String title;
 
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _DocList createState() => _DocList();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _DocList extends State<DocList> {
   int _counter = 0;
 
   void _incrementCounter() {
@@ -102,7 +84,7 @@ class _MyHomePageState extends State<MyHomePage> {
             ),
             Text(
               '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+              style: Theme.of(context).textTheme.display1,
             ),
           ],
         ),
